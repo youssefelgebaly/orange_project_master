f71696c425c19d320be3511bd0c709e6797f7188
youssefelgebaly <you@example.com>
2022-02-18 19:49:37 +0200

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:orange_project_master/layout/bloc/cubit.dart';
 import 'package:orange_project_master/layout/bloc/state.dart';
-import 'package:orange_project_master/layout/layout_screen.dart';
 import 'package:orange_project_master/modules/login_screen.dart';
 import 'network/dio_helper.dart';
 
@@ -15,7 +14,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-//first
+  //first
+  // new branch
+  // git commit -m 'new branch'
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
